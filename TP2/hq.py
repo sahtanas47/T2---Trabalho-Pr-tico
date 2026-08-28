30ce31876431d5440cd56e30c7985d90c0a79828
Sarah Cristina <sc8479164@gmail.com>
2026-08-28 20:12:35 -0300

    hq.py

--- TP2/hq.py
+++ TP2/hq.py
@@ -1,11 +1,12 @@
 from item import Item
 
 class HQ(Item):
-    def __init__(self, titulo, ano, edicao):
+    def __init__(self, titulo, ano, edicao, autor):
         super().__init__(titulo, ano)
         self.edicao = edicao
+        self.autor = autor
 
     def exibir_detalhes(self):
         base = super().exibir_detalhes()
-        return f"[HQ] {base} | Edição nº: {self.edicao}"
+        return f"[TIPO: HQ] {base} | Edição nº: {self.edicao} | Autor: {self.autor}"
     
